a7bc20ee14d91ee60926fa0975cd495e2a467d8e
Calvin Roberts <roberts.calvin85@gmail.com>
2022-11-18 22:07:11 +0000

    Create onclick function to signinBtn

---
+++ script.js
@@ -0,0 +1,8 @@
+let signupBtn = document.getElementById('signupBtn'),
+  signinBtn = document.getElementById('signinBtn'),
+  nameField = document.getElementById('nameField'),
+  title = document.getElementById('title');
+
+signinBtn.onclick = function () {
+  nameField.style.maxHeight = 0;
+};
